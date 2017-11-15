06796472814fad3a1c14a0b184d368185d74ddbe
Rob Speer <rob@luminoso.com>
2017-11-15 12:29:51 -0500

    put local files in modules/conceptnet/files

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 A Puppet module for setting up ConceptNet on a new machine.
 
 First, get the Puppet dependencies by running `sudo sh puppet-setup.sh`. Then
-Puppet should be able to do the rest, with `sudo puppet apply conceptnet.pp`.
+Puppet should be able to do the rest, with `sudo puppet apply manifests/conceptnet.pp`.
--- manifests/conceptnet-web.pp
+++ manifests/conceptnet-web.pp
@@ -5,30 +5,32 @@ package { 'nginx':
 }
 
 # Install the ConceptNet web package from source
-python::pip { 'conceptnet':
+python::pip { 'conceptnet_web':
   ensure     => 'latest',
   virtualenv => '/home/conceptnet/env',
   pkgname    => '/home/conceptnet/conceptnet5/web',
-  egg        => 'conceptnet-web',
+  egg        => 'conceptnet_web',
   install_args => '-e',
-  require    => Vcsrepo['/home/conceptnet/conceptnet5'],
+  require    => [ Vcsrepo['/home/conceptnet/conceptnet5'], Python::Pip['conceptnet'] ],
 }
 
 
 # Install web server config. First, nginx.
 file { '/etc/nginx/conf.d/conceptnet.conf':
   ensure  => 'present',
-  source  => 'config/nginx/conceptnet.conf',
+  source  => 'puppet:///modules/conceptnet/nginx/conceptnet.conf',
   require => Package['nginx'],
 }
 
 file { '/home/conceptnet/nginx':
   ensure  => 'directory',
+  require => User['conceptnet'],
 }
 
 # Remove Debian's "presumptuous" default Nginx configuration
 file { '/etc/nginx/sites-enabled/default':
   ensure  => 'absent',
+  require => Package['nginx'],
 }
 
 # The uWSGI processes need to be running, as well. Set up the uWSGI Emperor
@@ -41,6 +43,7 @@ python::pip { 'uwsgi':
 
 file { '/home/conceptnet/uwsgi':
   ensure  => 'directory',
+  require => User['conceptnet'],
 }
 
 file { '/home/conceptnet/uwsgi/run':
@@ -53,20 +56,20 @@ file { '/home/conceptnet/uwsgi/apps':
 
 file { '/home/conceptnet/uwsgi/emperor.ini':
   ensure  => 'present',
-  source  => 'config/uwsgi/emperor.ini',
+  source  => 'puppet:///modules/conceptnet/uwsgi/emperor.ini',
 }
 
 file { '/home/conceptnet/uwsgi/apps/conceptnet-web.ini':
   ensure  => 'present',
-  source  => 'config/uwsgi/conceptnet-web.ini',
+  source  => 'puppet:///modules/conceptnet/uwsgi/conceptnet-web.ini',
 }
 
 file { '/home/conceptnet/uwsgi/apps/conceptnet-api.ini':
   ensure  => 'present',
-  source  => 'config/uwsgi/conceptnet-api.ini',
+  source  => 'puppet:///modules/conceptnet/uwsgi/conceptnet-api.ini',
 }
 
 file { '/lib/systemd/system/conceptnet.service':
   ensure  => 'present',
-  source  => 'config/systemd/conceptnet.service',
+  source  => 'puppet:///modules/conceptnet/systemd/conceptnet.service',
 }
