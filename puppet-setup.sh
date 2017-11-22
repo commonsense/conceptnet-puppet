de4a395710d1d307be45fbddca786c3e432895f5
Rob Speer <rob@luminoso.com>
2017-11-22 11:08:25 -0500

    use Ubuntu's puppet modules so that they're compateble with Puppet 3.8

--- puppet-setup.sh
+++ puppet-setup.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 # Install Ubuntu's packages for Puppet. On Xenial, it gets v3.8.5.
-sudo apt-get install puppet-common
+sudo apt install puppet-common puppet-module-puppetlabs-postgresql puppet-module-puppetlabs-vcsrepo
 
-# Get Puppet module that lets us use apt commands
-sudo puppet module install puppetlabs-apt
-# Get Puppet module that lets us setup postgresql
-sudo puppet module install puppetlabs-postgresql
-# Get Puppet module that lets us pull repos
-sudo puppet module install puppetlabs-vcsrepo
 # Get the Puppet module that lets us work with Python and Pip.
 sudo puppet module install stankevich-python
 
